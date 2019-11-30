f26c72b530bcac77986c577f2015f68a9656e5ab
Depaepe Jean-Michel <AJA063@engie.com>
2019-11-30 20:11:01 +0100

    personnage packman modélisé de manière simple

--- eu/epfc/c5255/packman/App.java
+++ eu/epfc/c5255/packman/App.java
@@ -14,13 +14,28 @@ public class App extends PApplet {
 	/**
 	 * nombre de cellules par damier
 	 */
-	static int nrCells = 28;
+	static int nrCells = 29;
 	
 	/**
 	 * taille des cellules
 	 */
 	static int nrPixelPerCell = 21;
 
+	/**
+	 * taille de packman
+	 */
+	static int packmanDiameter = (int) (nrPixelPerCell * 1.1);
+	
+	/**
+	 * absisse (x) de packman (première cellule = 0)
+	 */
+	static int packmanX = nrCells / 2;
+	
+	/**
+	 * coordonnée (y) de packman (première cellule = 0)
+	 */
+	static int packmanY = 10;
+	
 	/**
 	 * point d'entrée du programme
 	 * @param args sont les arguments passés par la ligne de commande
@@ -48,10 +63,16 @@ public class App extends PApplet {
 	 */
 	@Override
 	public void setup () {
+	}
+	
+	@Override
+	public void draw () {
 		// couleur de fond noir
 		background(0);
+		
 		// pinceau de trait mauve
 		stroke(128, 0, 255);
+		
 		// dessine le damier
 		for (int i = 0; i <= nrCells; ++i) {
 			// ligne horizontale
@@ -59,11 +80,46 @@ public class App extends PApplet {
 			// ligne verticale
 			line(margin + i * nrPixelPerCell, margin, margin + i * nrPixelPerCell, margin + nrCells * nrPixelPerCell);
 		}
+		
+		drawPackman();		
 	}
 	
+	/**
+	 * calcule la nouvelle position de packman
+	 */
 	@Override
-	public void draw () {
-		
+	public void keyPressed () {
+		// dépendant la touche clavier enfoncée
+		switch (keyCode) {
+		case UP:
+			packmanY = --packmanY % nrCells;
+			break;
+		case RIGHT:
+			packmanX = ++packmanX % nrCells;
+			break;
+		case DOWN:
+			packmanY = ++packmanY % nrCells;
+			break;
+		case LEFT:
+			packmanX = --packmanX % nrCells;
+			break;
+		}
+		// appliquer une translation de nrCells si négatif
+		if (packmanX < 0) packmanX +=nrCells;
+		if (packmanY < 0) packmanY +=nrCells;
 	}
-
+	
+	/**
+	 * dessine packman
+	 */
+	public void drawPackman () {
+		// pas de trait
+		noStroke();
+		
+		// pinceau de remplissage jaune
+		fill(255, 255, 0);
+		
+		// dessine packman
+		ellipse(margin + (packmanX + 0.5F) * nrPixelPerCell, margin + (packmanY + 0.5F) * nrPixelPerCell, packmanDiameter, packmanDiameter);
+	}
 }

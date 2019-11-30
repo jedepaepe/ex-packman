8fbb3c2be6e529744baa24ce1e9f82a403b5b931
Depaepe Jean-Michel <AJA063@engie.com>
2019-11-30 19:21:12 +0100

    ajout de commentaires pour expliquer le code

--- eu/epfc/c5255/packman/App.java
+++ eu/epfc/c5255/packman/App.java
@@ -4,33 +4,61 @@ import processing.core.PApplet;
 
 /**
  * Classe principale de l'application
- * 
  */
 public class App extends PApplet {
+	/**
+	 * taille de la marge ne pixels
+	 */
 	static int margin = 25;
+	
+	/**
+	 * nombre de cellules par damier
+	 */
 	static int nrCells = 28;
+	
+	/**
+	 * taille des cellules
+	 */
 	static int nrPixelPerCell = 21;
 
+	/**
+	 * point d'entrée du programme
+	 * @param args sont les arguments passés par la ligne de commande
+	 */
 	public static void main(String[] args) {
-		// start GUI
+		// démarre le moteur Processing (la GUI)
 		PApplet.main(App.class.getName());
 	}
 	
+	/**
+	 * configure l'application
+	 */
 	@Override
 	public void settings () {
+		/**
+		 * la taille de la fenêtre est:
+		 * largeur = marge + nr_de_cellules * taille_de_la_cellule + marge
+		 * hauteur = marge + nr_de_cellules * taille_de_la_cellule + marge
+		 */
 		size(nrCells * nrPixelPerCell + 2 * margin, nrCells * nrPixelPerCell + 2 * margin);
 	}
 	
+	/**
+	 * dessine l'image de base
+	 */
 	@Override
 	public void setup () {
+		// couleur de fond noir
 		background(0);
+		// pinceau de trait mauve
 		stroke(128, 0, 255);
-		// draw panel
+		// dessine le damier
 		for (int i = 0; i <= nrCells; ++i) {
+			// ligne horizontale
 			line(margin, margin + i * nrPixelPerCell, margin + nrCells * nrPixelPerCell, margin + i * nrPixelPerCell);
+			// ligne verticale
 			line(margin + i * nrPixelPerCell, margin, margin + i * nrPixelPerCell, margin + nrCells * nrPixelPerCell);
 		}
-		
 	}
 	
 	@Override
